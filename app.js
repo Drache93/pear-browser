82e67f4cb919edbf6baea4e6065a164f11cc622a
Dominic Cassidy <dominic@koroutine.tech>
2025-08-22 14:38:25 +0100

    feat: load app

--- app.js
+++ app.js
@@ -3,8 +3,15 @@
 import SchemaScheets from 'schema-sheets'
 import Corestore from 'corestore'
 import Alpine from 'alpinejs'
+import htmx from 'htmx.org';
+import * as PearRequest from 'pear-request'
+import defaultApps from './apps.json' assert { type: 'json' }
 
 window.Alpine = Alpine
+window.htmx = htmx
+
+htmx.logAll();
+
 Alpine.start()
 
 const store = new Corestore("./test")
@@ -38,23 +45,25 @@ const schemas = await sheets.listSchemas().then(schemas => schemas.reduce((acc, 
     return acc
 }, {}))
 
+global.PearRequest = PearRequest
 global.schemas = schemas
 global.sheets = sheets
 
 let apps = await sheets.list(schemas.apps)
 
-if (apps.length === 0) {
-    const success = await sheets.addRow(schemas.apps, {
-        name: 'Keet Gifs',
-        icon: 'https://docs.pears.com/~gitbook/image?url=https%3A%2F%2F1301247912-files.gitbook.io%2F%7E%2Ffiles%2Fv0%2Fb%2Fgitbook-x-prod.appspot.com%2Fo%2Fspaces%252Fjdab83NqbLtX0WX9qY2n%252Flogo%252FNZwXgspge0TwHeeUwAFo%252Fpear-vector.svg%3Falt%3Dmedia%26token%3D50bb2ece-f774-47b7-a604-441ccf9aeb8c&width=260&dpr=4&quality=100&sign=477f992b&sv=2',
-        description: 'Share your favorite GIFs and WebP images directly with friends using peer-to-peer technology. No accounts, no upload limits, no corporate overlords.',
-        url: 'pear://eedfdkadkz96r9kh1pc9fyjr6h986i76hy78s5yszg9a85ee7g3o'
-    }, Date.now())
+if (apps.length < defaultApps.length) {
+    console.log("app", defaultApps.length)
+    for (const a of defaultApps) {
+        const apps = await sheets.list(schemas.apps, {
+            query: `[?url = '${a.url}']`,
+        });
 
-    if (success) {
-        console.log('App added successfully')
-        apps = await sheets.list(schemas.apps)
-    } else {
-        console.log('Failed to add app')
+        if (apps.length !== 0) {
+            continue
+        }
+        const success = await sheets.addRow(schemas.apps, a, Date.now())
+        if (!success) {
+            throw new Error(`Failed to add default app ${a.name}`)
+        }
     }
 }
