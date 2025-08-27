0c70977ae0d5dc0c0d6d15654bf160b74cf84515
Dominic Cassidy <dominic@koroutine.tech>
2025-08-27 16:23:30 +0100

    chore: startup logging

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ import BlindPeering from 'blind-peering'
 import Hyperswarm from 'hyperswarm'
 import Wakeup from 'protomux-wakeup'
 
+const log = (...args) => {
+    process.stderr.write(args.join(" ") + "\n")
+}
+
 const blindPeers = ["3x4bak4wh5tar1w3ai5h7ixipq8gpagifggag83k1xetjmhqynxo"]
 
 window.Alpine = Alpine
@@ -29,30 +33,57 @@ const freshStart = Pear.config.args.includes("--fresh")
 const keyValue = Pear.config.args.find(arg => arg.startsWith("--key="))
 const key = keyValue ? b4a.from(keyValue.split("=")[1], 'hex') : b4a.from('0d5336f6ce7fa12c717cae34ba7a1e956c98bb971eff6dd6dcf3b2819f0c5a15', 'hex')
 
+log("Setting up store...")
+
 const store = new Corestore(Pear.config.storage)
 const swarm = new Hyperswarm()
 const wakeup = new Wakeup()
-const sheets = new SchemaScheets(store, key)
-await sheets.ready()
-
 const blind = new BlindPeering(swarm, store, { wakeup, mirrors: blindPeers })
+const sheets = new SchemaScheets(store, key)
+
+log("Preparing search engine...")
+
+await sheets.ready()
+// log("Search engine key:", b4a.toString(sheets.key, 'hex'))
+
+log("Joining swarm...")
+swarm.join(sheets.base.discoveryKey)
+
+log("Adding autobase background...")
 blind.addAutobaseBackground(sheets.base)
 
-console.log("key", b4a.toString(sheets.key, 'hex'))
+log("Setting up history...")
 
 const history = new History(store)
 await history.ready()
 
-Pear.teardown(() => sheets.close())
-
-if (!await sheets.joined()) {
-    const name = b4a.toString(crypto.randomBytes(32), 'hex')
-    await sheets.join(name)
+const teardown = async () => {
+    await sheets.close()
+    await blind.close()
+    await swarm.destroy()
+    await store.close()
 }
+
+
+swarm.on('connection', c => {
+    log("Connected with peer")
+    c.on('close', function () { })
+    store.replicate(c)
+    wakeup.addStream(c)
+})
+
+process.once('SIGINT', async function () {
+    log('shutting down....')
+    await teardown()
+    process.exit()
+})
+
+log("Joining search engine...")
+await sheets.join()
 
 // For testing
 if (freshStart) {
-    console.log(sheets)
+    log(sheets)
 
     const appsSchemaId = await sheets.addNewSchema('apps', {
         type: 'object',
@@ -64,7 +95,7 @@ if (freshStart) {
         },
         required: ['name', 'icon', 'description', 'url']
     })
-    console.log(appsSchemaId)
+    log(appsSchemaId)
 }
 
 const schemas = await sheets.listSchemas().then(schemas => schemas.reduce((acc, schema) => {
@@ -81,13 +112,13 @@ if (freshStart) {
     let apps = await sheets.list(schemas.apps)
 
     if (apps.length < defaultApps.length) {
-        console.log("app", defaultApps.length)
+        log("app", defaultApps.length)
         for (const a of defaultApps) {
             const apps = await sheets.list(schemas.apps, {
                 query: `[].{url: url}`,
             });
 
-            console.log("result", apps)
+            log("result", apps)
 
             if (apps.find(app => app.url === a.url) !== undefined) {
                 continue
@@ -101,6 +132,7 @@ if (freshStart) {
     }
 }
 
+log("Ready")
 // Fire off event to say ready
 document.dispatchEvent(new Event('pear-browser-ready'))
 globalThis.pearBrowserReady = true
